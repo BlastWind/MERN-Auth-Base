592616269a4a453af24d1c5b4baaa6d2da33d40f
Andrew Chen <andrewchen14250@gmail.com>
2020-03-01 15:49:52 -0500

    Initial Commit

---
+++ src/client/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
+
+export default class App extends Component {
+  state = {};
+
+  componentDidMount() {}
+
+  render() {
+    return <div>Time to Start working on the frontend!</div>;
+  }
+}
---
+++ src/server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "your_mongoDB_key_here",
+  secretOrKey: "secret"
+};
